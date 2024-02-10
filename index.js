800d0304cbcbc35e58877d6f3698477246404111
dverves <denver1033@gmail.com>
2024-02-10 15:47:39 +0200

    setting up main blogs

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ import morgan from 'morgan';
 
 const port = 8080;
 const app  = express();
+const uName = "";
+const uEmail = "";
+const uPass = "";
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({extended:true}));
@@ -13,6 +16,29 @@ app.use(bodyParser.urlencoded({extended:true}));
 
 app.get("/",(req,res)=>{
     res.status(200).render("index.ejs")
+})
+
+app.get("/login",(req,res)=>{
+    res.status(200).render("login.ejs");
+});
+
+app.get("/about",(req,res)=>{
+    res.status(200).render("about.ejs")
+})
+
+app.get("/fqs",(req,res)=>{
+    res.status(200).render("fqs.ejs")
+})
+
+app.post("/",(req,res)=>{
+    let data = {
+        uName: req.body["name"],
+        uEmail: req.body["email"],
+        uPass: req.body["pass"]
+    }
+    console.log(data)
+    res.status(200).render("index.ejs",{data})
+
 })
 
 app.listen(port,(err)=>{
